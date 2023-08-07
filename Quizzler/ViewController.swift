35936c3f8667cf9ddc6c5bb7a3feeea878443d4c
Burak Can Karasel <99825584+burakkarasel@users.noreply.github.com>
2023-08-07 10:20:50 +0300

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Burak Karasel on 7.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
